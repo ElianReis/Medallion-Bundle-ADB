9e3e1d101ad1748c92d081070e529f3ddeaeb3a4
ElianReis <elian.muluk@gmail.com>
2024-02-22 21:11:53 -0300

    Making the example test slightly better with unittest and azure pipelines

--- tests/main.py
+++ tests/main.py
@@ -1,10 +1,18 @@
 from databricks.connect import DatabricksSession
 from pyspark.sql import SparkSession
+import unittest
 
 SparkSession.builder = DatabricksSession.builder
 SparkSession.builder.getOrCreate()
 
-def test_main():
-    # Still pending usage of unit tests in datasets
-    addition = 1 + 1
-    assert addition == 2
+
+class TestExample(unittest.TestCase):
+    def test_addition(self):
+        self.assertEqual(1 + 1, 2)
+
+    def test_subtraction(self):
+        self.assertEqual(3 - 2, 1)
+
+
+if __name__ == "__main__":
+    unittest.main()
